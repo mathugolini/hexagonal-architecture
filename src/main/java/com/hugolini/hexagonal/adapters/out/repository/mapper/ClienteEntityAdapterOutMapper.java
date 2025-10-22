d713f87f63a36a8de6eb3ae06685b6c8c86a28ea
mathugolini <mathugolini@gmail.com>
2025-10-21 22:15:22 -0300

    adapter inseracao do cliente

---
+++ src/main/java/com/hugolini/hexagonal/adapters/out/repository/mapper/ClienteEntityAdapterOutMapper.java
@@ -0,0 +1,11 @@
+package com.hugolini.hexagonal.adapters.out.repository.mapper;
+
+import com.hugolini.hexagonal.adapters.out.repository.entity.ClienteAdapterOutEntity;
+import com.hugolini.hexagonal.application.core.domain.ClienteDomain;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ClienteEntityAdapterOutMapper {
+
+    ClienteAdapterOutEntity toEntity(ClienteDomain cliente);
+}
